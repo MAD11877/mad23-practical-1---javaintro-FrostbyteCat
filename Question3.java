52aee53ccb2887c879d6b9aa84935972a99ee14e
CuriousCatxolotl <andrewrimington723@gmail.com>
2023-04-18 14:26:14 +0800

    Week 1 Practical - Andrew Rimington

--- Question3.java
+++ Question3.java
@@ -9,8 +9,25 @@ public class Question3
      * 
      * Hint: in.nextInt() to read integer
      */
-     
-    Scanner in = new Scanner(System.in);
-    
+    int user_int;
+    Scanner scanner = new Scanner(System.in);
+
+    while(true){
+      System.out.print("Enter an integer to calculate the result when it multiplies itself: ");
+
+      if(scanner.hasNextInt()){
+        user_int = scanner.nextInt();
+      }
+      else{
+        System.out.println("Invalid input. Please enter an integer.");
+        scanner.next();
+        continue;
+      }
+      break;
+    }
+
+    scanner.close();
+    int result = user_int * user_int;
+    System.out.printf("The result of %d when multiplied by itself is %d.", user_int, result);
   }
 }
